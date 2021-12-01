c56b90fc37149f9a4608697d9d2e17be5c898922
Colin Brislawn <cbrisl@gmail.com>
2021-12-01 16:23:45 -0500

    Change order of recipe/build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,9 +2,9 @@
 
 export DISABLE_AUTOBREW=1
 
-# shellcheck disable=SC2086
-${R} CMD INSTALL --build . ${R_ARGS}
-
 # Get an updated config.sub and config.guess
 cp $BUILD_PREFIX/share/gnuconfig/config.* .
+
+# shellcheck disable=SC2086
+${R} CMD INSTALL --build . ${R_ARGS}
 
